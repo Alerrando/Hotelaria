4846ca4ce60cc80d37c05e071bf5d283bd78eef3
Alerrando Breno <alerrando2@gmail.com>
2022-12-03 22:54:38 -0300

    hiding menu on mobile devices

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  menu:boolean = this.windowsToogle();
 
+
+  menuToogle(){
+    this.menu == true ? this.menu = false : this.menu = true;
+    console.log(this.menu);
+  }
+
+  windowsToogle(){
+    return window.innerWidth >= 768 ? true : false
+  }
 }
