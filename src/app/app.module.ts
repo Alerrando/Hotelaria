b9911dadc776dc46bc25ffb70259880c76a89f29
Alerrando Breno <alerrando2@gmail.com>
2022-12-05 11:48:10 -0300

    fixing bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
