972f030ffe8ace49f38c257a38bf0b1301217fb4
Alerrando Breno <alerrando2@gmail.com>
2022-12-06 13:08:49 -0300

    add images login

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sombreado': 'rgba(0, 0, 0, 0.3)'
+      }
+    },
   },
   plugins: [],
 }
