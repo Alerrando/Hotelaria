d1b1966f7bb4b09dbe8bd30d891f9b4c560323b1
Alerrando Breno <alerrando2@gmail.com>
2022-12-07 15:45:39 -0300

    add route registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../home/home.component.scss']
 })
 export class RegisterComponent {
 
