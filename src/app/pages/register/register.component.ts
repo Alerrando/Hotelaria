55d82c824ec8dd94562b5e7660783446a4d696cd
Alerrando Breno <alerrando2@gmail.com>
2022-12-07 16:22:16 -0300

    page registro complete

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss', '../home/home.component.scss']
+  styleUrls: ['./register.component.scss', '../home/home.component.scss', '../login/login.component.scss']
 })
 export class RegisterComponent {
 
